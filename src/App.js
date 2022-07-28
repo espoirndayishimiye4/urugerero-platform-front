45d734d5395b50647a36b750645ddd76d183bfe9
unknown <tetronadiaparquer@gmail.com>
2022-07-28 17:12:17 +0200

    feat: login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Logo from "./image/Logo.jpeg"
+import email from "./image/email.jpg"
+import Lock from "./image/Lock.jpg"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+          <span>Urugerero Platform</span>
+            <div className="container-image">
+              
+
+            </div>
+
+          </div>
+          <div>
+            <h1>login page</h1>
+            <div>
+            <img src={email} alt="email" className="email"/>
+            <input type="text" placeholder="email" className="name"/>
+            </div>
+
+            <div className="second-input">
+            <img src={Lock} alt="Lock" className="Lock"/>
+            <input type="password" placeholder="password" className="name"/>
+            </div>
+            <div className="login-button">
+            <button>Login</button>
+            </div>
+
+           
+
+            <p className="link">
+              <a href="#">Forgot password ? </a> or <a href="#">Sign up</a>
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
-  );
+  )
+    
+  
 }
 
 export default App;
